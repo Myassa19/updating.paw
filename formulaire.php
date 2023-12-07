2abfb66edf4f1a2f5744640ed02b30603415c076
Myassa maallem <129672284+Myassa19@users.noreply.github.com>
2023-12-07 17:13:26 +0100

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,45 +9,68 @@
 </head>
 
 <body style="background-image:url(images/image4.jpg)">
-    <div class="container mt-5">
-        <h1 class="text-center">FORMULAIRE DE RECOURS</h1>
-        <form method="post" action="Api.php">
-    	<div class="container mt-5 pt-5">
-        	<div class="row">
-            <div class="col-12 col-sm-8 col-md-6 m-auto">
-                    <div class="form-group">
-                        <label for="ID" class="font-weight-bold">ID</label>
-                        <input type="text" class="form-control" name="ID">
+    <section>
+        <div class="container mt-5 pt-5">
+            <div class="row">
+                <div class="col-12 col-sm-8 col-md-6 m-auto">
+                    <div class="card">
+                        <div class="card-body btn-primary">
+                            <div class="text-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"
+                                    class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path
+                                        d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                    <path fill-rule="evenodd"
+                                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                                </svg>
+                            </div>
+                            <div class="container mt-3">
+                                <h1 class="text-center">FORMULAIRE DE RECOURS</h1>
+                                <form method="post" action="Api.php">
+                                    <div class="container mt-1 pt-1">
+                                        <div class="row">
+                                            <div class="col-12 col-sm-10 col-md-12 m-auto">
+                                                <div class="mb-2">
+                                                    <label for="ID" class="form-label font-weight-bold">ID</label>
+                                                    <input type="text" class="form-control" name="ID">
+                                                </div>
+                                                <div class="mb-2">
+                                                    <label for="MODULE"
+                                                        class="form-label font-weight-bold">MODULE</label>
+                                                    <input type="text" class="form-control" name="MODULE">
+                                                </div>
+                                                <div class="mb-2">
+                                                    <label for="NATURE"
+                                                        class="form-label font-weight-bold">NATURE (CC, Examen)</label>
+                                                    <select id="NATURE" name="NATURE" class="form-control">
+                                                        <option value="CC">CC</option>
+                                                        <option value="Examen">Examen</option>
+                                                    </select>
+                                                </div>
+                                                <div class="mb-2">
+                                                    <label for="NOTE-AFFICHE"
+                                                        class="form-label font-weight-bold">NOTE AFFICHE</label>
+                                                    <input type="number" class="form-control" name="NOTE-AFFICHE">
+                                                </div>
+                                                <div class="mb-2">
+                                                    <label for="NOTE-REEL"
+                                                        class="form-label font-weight-bold">NOTE REEL</label>
+                                                    <input type="number" class="form-control" name="NOTE-REEL">
+                                                </div>
+                                                <button type="submit" class="btn btn-dark">Envoyer</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="MODULE" class="font-weight-bold">MODULE</label>
-                        <input type="text" class="form-control" name="MODULE">
-                    </div>
-                    <div class="form-group">
-                        <label for="NATURE" class="font-weight-bold">NATURE (CC, Examen)</label>
-                        <select id="NATURE" name="NATURE" class="form-control">
-                            <option value="CC">CC</option>
-                            <option value="Examen">Examen</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="NOTE-AFFICHE" class="font-weight-bold">NOTE AFFICHE</label>
-                        <input type="number" class="form-control" name="NOTE-AFFICHE">
-                    </div>
-                    <div class="form-group">
-                        <label for="NOTE-REEL" class="font-weight-bold">NOTE REEL</label>
-                        <input type="number" class="form-control" name="NOTE-REEL">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Envoyer</button>
                 </div>
             </div>
-</div>
-</div>
-</div>
-        </form>
-    </div>
-
+        </div>
+    </section>
     <!-- Bootstrap JS and other scripts if needed -->
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
